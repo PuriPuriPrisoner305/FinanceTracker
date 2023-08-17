d846250c7baba83652efa11232f5944a68595b50
PuriPuriPrisoner305 <v1.ardy1@gmail.com>
2023-08-17 17:41:49 +0700

    Initial Commit

---
+++ FinanceTracker/Modules/TabBar/CustomTabBar.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTabBar.swift
+//  FinanceTracker
+//
+//  Created by Ardyansyah Wijaya on 17/08/23.
+//
+
+import UIKit
+
+class CustomTabBar: UITabBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
